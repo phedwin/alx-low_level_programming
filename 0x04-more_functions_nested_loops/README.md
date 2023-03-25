f3d4b095e50733b75a031075e6c397d6a52278e2
phedwin <economistphedwinnne@gmail.com>
2023-03-25 22:53:16 +0000

    more on functions and nested loops ~advanced

--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-0x04. C - More functions, more nestedloops
+**More functions, more nested loops**
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,10 +1,10 @@
-#include "main.h""
-include <unistd.h>
+#include <unistd.h>
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: On Success 1;
- * On error, -1 is returned, and errno is set appropriatedly
+ * Return 1 if true, 0 if false
+ * On error, -1 return.
  */
 int _putchar(char c)
 {
