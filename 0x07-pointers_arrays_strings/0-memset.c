5583e69b329e16de9e0b51cf131c08ea9b489b45
phedwin <economistphedwinnne@gmail.com>
2023-04-03 22:07:20 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+/**#include "holberton.h"
 
 /**
  * *_memset - fills memory with a constant byte.
@@ -19,3 +19,5 @@ char *_memset(char *s, char b, unsigned int n)
 
 	return (s);
 }
+**/
+	
