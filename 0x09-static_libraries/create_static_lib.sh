5e5e2d6618c940080ab62b691ff77d6ad75035c3
phedwin <economistphedwinnne@gmail.com>
2023-04-07 20:25:55 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-gcc -c *.c
+C_FILES=$(find . -maxdepth 1 -type f -name "*.c")
 
-ar rc liball.a *.o
-
-ranlib liball.a
+ar rcs liball.a $(for file in $C_FILES; do gcc -c $file; echo ${file%.c}.o; done)
